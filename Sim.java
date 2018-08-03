f770c2c1863d787b3d8b56b5ac3376691bd6146a
Aidan <32269894+ajz2000@users.noreply.github.com>
2018-08-03 17:33:46 -0400

    Implemented basic window, fixed gitignore

---
+++ Sim.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+public class Sim extends JFrame{
+
+  public static void main(String args[]){
+    new Sim();
+  }
+
+  public Sim(){
+    JPanel tempPanel = new JPanel();
+    tempPanel.setPreferredSize(new Dimension(1920,1080));
+    tempPanel.setBackground(new Color(35,35,35));
+    add(tempPanel, BorderLayout.CENTER);
+    
+  setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setUndecorated(true);
+    pack();
+    setLocationRelativeTo(null);
+    setTitle("Sim");
+    setVisible(true);  
+  }
+  
+}
